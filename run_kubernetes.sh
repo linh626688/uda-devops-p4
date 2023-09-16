30fef57994fac82fd5a910a057df531fa64622d2
linh626688 <linh626688@gmail.com>
2023-09-16 02:54:42 +0000

    step 7

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=linh626688/housing-price-predication
 
 # Step 2
 # Run the Docker Hub container with kubernetes
 
+kubectl run housing-price-predication --image=$dockerpath:latest --port=80
+
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward pod/housing-price-predication 8000:80
 
